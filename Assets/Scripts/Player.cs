9207f2f05df56b264aec5491ac96cfb926876b88
Jacek <placebo91@gmail.com>
2017-06-25 17:21:00 +0200

    VERY BASIC player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rigidBody;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = input * speed;
+        rigidBody.velocity = velocity;
+    }
+}
